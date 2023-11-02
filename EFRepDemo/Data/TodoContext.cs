1c991fd342bd381c9bba9cc51974ad52efa31e67
christoffer.fjellstrom <christoffer.fjellstrom@qlok.se>
2023-11-02 12:01:06 +0100

    Add project files.

---
+++ EFRepDemo/Data/TodoContext.cs
@@ -0,0 +1,21 @@
+﻿using EFRepDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFRepDemo.Data
+{
+    internal class TodoContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\.;Initial Catalog=Todo;Integrated Security=True;Pooling=False");
+        }
+    }
+}
---
+++ EFRepDemo/Program.cs
@@ -0,0 +1,94 @@
+﻿using EFRepDemo.Data;
+using EFRepDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFRepDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(TodoContext context = new TodoContext())
+            {
+                User user = UserMenu(context);
+                TodoMenu(context, user);
+            }
+        }
+
+        static User UserMenu(TodoContext context)
+        {
+            Console.Clear();
+
+            // Hämta alla users
+            List<User> users = context.Users.ToList();
+            for (int i = 0; i < users.Count; i++)
+            {
+                Console.WriteLine($"{i}. {users[i].Name}");
+            }
+            Console.WriteLine("c to create new user");
+            string input = Console.ReadLine();
+
+            if (input == "c")
+            {
+                Console.Write("Enter user name:");
+                string name = Console.ReadLine();
+                User user = new User()
+                {
+                    Name = name
+                };
+                context.Users.Add(user);
+                context.SaveChanges();
+                return user;
+            }
+
+            int menuChoice = Convert.ToInt32(input);
+            return users[menuChoice];
+        }
+
+        static void TodoMenu(TodoContext context, User user)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"{user.Name}s todo list");
+
+                // Hämta vår User och plocka ut dess TodoItems
+                List<TodoItem> todoItems = context.Users
+                    .Where(u => u.Id == user.Id)
+                    .Include(u => u.TodoItems) 
+                    .Single()
+                    .TodoItems
+                    .ToList();
+
+                for (int i = 0; i < todoItems.Count; i++)
+                {
+                    Console.WriteLine($"{i}. {todoItems[i].Title}: {todoItems[i].Description}");
+                }
+                Console.WriteLine("c to create new todo item");
+                Console.WriteLine("q to quit");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "c":
+                        Console.Write("Enter title:");
+                        string title = Console.ReadLine();
+                        Console.Write("Enter description:");
+                        string description = Console.ReadLine();
+                        TodoItem newItem = new TodoItem()
+                        {
+                            Title = title,
+                            Description = description,
+                            User = user
+                        };
+                        context.TodoItems.Add(newItem);
+                        context.SaveChanges();
+                        break;
+                    case "q":
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+    }
+}
